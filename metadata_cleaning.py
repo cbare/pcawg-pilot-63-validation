3da1b79c6dd79d5aa7a6d5496cc98d72c7cb8cdd
Christopher Bare <chris.bare@sagebase.org>
2015-02-14 12:05:39 -0800

    updated to add more sources and do some metadata cleaning

---
+++ metadata_cleaning.py
@@ -0,0 +1,116 @@
+
+
+# df = query2df(syn.chunkedQuery('select * from file where benefactorId=="%s"' % DATA_SOURCE_PROJECT_ID))
+
+annotation_keys = ['center', 'center_name', 'workflow_name', 'variant_workflow',
+                   'call_type', 'dataType', 'dataSubType', 'variant_type', 'fileType']
+
+for df in dfs:
+    series = df.apply(lambda x: Counter(x).most_common())
+    print
+    print series['source']
+
+    for key in annotation_keys:
+        if key in series:
+            print key, '=', series[key]
+
+
+pd.crosstab(df_all['center'].replace(float('NaN'), 'NA'), df_all['center_name'].replace(float('NaN'), 'NA'))
+
+pd.crosstab(df_all['center'].replace(float('NaN'), 'NA'), df_all['workflow_name'].replace(float('NaN'), 'NA'))
+
+pd.crosstab(df_all['center'].replace(float('NaN'), 'NA'), df_all['call_type'].replace(float('NaN'), 'NA'))
+pd.crosstab(df_all['center'].replace(float('NaN'), 'NA'), df_all['dataSubType'].replace(float('NaN'), 'NA'))
+
+
+
+
+def find_sample_id_source(sample_id):
+    results = []
+    for col in sample_df.columns:
+        if sample_id in sample_df[col].values:
+            results.append(col)
+    return tuple(results)
+
+
+results = syn.tableQuery('select * from syn2887105')
+sample_df = results.asDataFrame()
+
+## figure out which centers named their samples with which IDs
+sample_id_sources = df_all.sample_id.apply(find_sample_id_source)
+pd.crosstab(sample_id_sources, df_all.source)
+
+# In [254]: pd.crosstab(sample_id_sources, df_all.source)
+# Out[254]: 
+# source                                   BSC  Broad  DKFZ  EMBL  MDA_HGSC  \
+# sample_id                                                                   
+# ()                                         0      0     0     0         0   
+# (Donor ID,)                                0      0     0     0         0   
+# (Normal Analysis ID,)                      0      0     0     0         0   
+# (Tumour Analysis ID,)                    567    998  1068   252       108   
+# (Tumour Analyzed Sample/Aliquot GUUID,)    0      0     0     0         0   
+
+# source                                   MDA_KChen  McGill  OICR  SANGER  SFU  \
+# sample_id                                                                       
+# ()                                               0       0     0    9024    0   
+# (Donor ID,)                                      0       0     0       0    0   
+# (Normal Analysis ID,)                            0       0     0       0    0   
+# (Tumour Analysis ID,)                          216      78   252       0  126   
+# (Tumour Analyzed Sample/Aliquot GUUID,)          0       0     0     896    0   
+
+# source                                   UCSC  WUSTL  Yale  
+# sample_id                                                   
+# ()                                          0      6     0  
+# (Donor ID,)                               165      0     0  
+# (Normal Analysis ID,)                       0      0    59  
+# (Tumour Analysis ID,)                       0    358     0  
+# (Tumour Analyzed Sample/Aliquot GUUID,)     0      0     0  
+
+
+## fix UCSC sample_id -> donor_id and Tumour Analysis ID -> analysis_id_tumor
+results = clean_query_results(syn.chunkedQuery('select id, name from file where parentId=="syn3107237"'))
+for result in results:
+    print result['id'], result['name']
+    e = syn.get(result['id'], downloadFile=False)
+    a = e.annotations
+    a['donor_id'] = a['sample_id']
+    a['analysis_id_tumor'] = sample_df['Tumour Analysis ID'][ sample_df['Donor ID']==e.sample_id[0] ][0]
+    print syn.setAnnotations(e, a)
+
+
+## fix sample_id -> analysis_id_tumor and Donor ID -> donor_id
+for source in sources:
+    if source.name in ["BSC", "Broad", "DKFZ", "EMBL", "MDA_HGSC", "MDA_KChen", "McGill", "OICR", "SFU", "WUSTL"]:
+        results = clean_query_results(syn.chunkedQuery('select id, name from file where parentId=="%s"' % source.folder_id))
+        for result in results:
+            if (result['id'] not in already_done) and (result['id'] not in second_batch_done):
+                print result['id'], result['name']
+                e = syn.get(result['id'], downloadFile=False)
+                a = e.annotations
+                a['analysis_id_tumor'] = a['sample_id']
+                donor_id_series = sample_df['Donor ID'][ sample_df['Tumour Analysis ID']==e.sample_id[0] ]
+                if len(donor_id_series) == 0:
+                    print "no donor ID associated with sample_id: ", a['sample_id']
+                else:
+                    a['donor_id'] = donor_id_series[0]
+                print syn.setAnnotations(e, a)
+
+
+for source in sources:
+    if source.name in ["Yale"]:
+        results = clean_query_results(syn.chunkedQuery('select id, name from file where parentId=="%s"' % source.folder_id))
+        for result in results:
+            if (result['id'] not in already_done) and (result['id'] not in second_batch_done):
+                print result['id'], result['name']
+                e = syn.get(result['id'], downloadFile=False)
+                a = e.annotations
+                a['analysis_id_tumor'] = a['sample_id']
+                if 'donor_id' not in a:
+                    donor_id_series = sample_df['Donor ID'][ sample_df['Normal Analysis ID']==e.sample_id[0] ]
+                    if len(donor_id_series) == 0:
+                        print "no donor ID associated with sample_id: ", a['sample_id']
+                    else:
+                        a['donor_id'] = donor_id_series[0]
+                print syn.setAnnotations(e, a)
+
+
--- pilot-63-progress.py
+++ pilot-63-progress.py
@@ -13,31 +13,6 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 
-syn = None
-
-## Project: ICGC-TCGA Whole Genome Pan-Cancer Analysis
-## /Files/VCF files/  syn2351328/syn2882202
-class Source(object):
-    def __init__(self, name, workflow, folder_id, prefix):
-        self.name = name
-        self.workflow = workflow
-        self.folder_id = folder_id
-        self.prefix = prefix
-    def __repr__(self):
-        return "Source(name=\"{name}\", workflow=\"{workflow}\", folder_id=\"{folder_id}\", prefix=\"{prefix}\")".format(**self.__dict__)
-
-sources = [
-    Source("DKFZ",   "DKFZ",        "syn3104289", ".somatic"),
-    Source("EMBL",   "delly",       "syn3153529", ".somatic"),
-    Source("SANGER", "svcp",        "syn3155834", ".somatic"),
-    Source("UCSC",   "gatk-muse",   "syn3107237", ".gatk_muse_0.9.9.5"),
-    Source("UCSC",   "gatk-mutect", "syn3107237", ".gatk_mutect"),
-    Source("UCSC",   "muse",        "syn3107237", ".muse_0.9.9.5")]
-
-PROJECT_SYNAPSE_ID     = "syn2875157"
-THIS_SCRIPT_SYNAPSE_ID = "syn3159349"
-BAR_CHART_SYNAPSE_ID   = "syn3158825"
-TABLE_SYNAPSE_ID       = "syn3159075"
 
 ## used to remove properties from Synapse query results
 property_keys= ['id', 'name', 'description',
@@ -55,17 +30,28 @@ def unlist_singletons(value):
         return value
 
 def clean_key(key):
-    return re.sub(r'(entity\.|file\.|folder\.|project\.|data\.)','', key)
+    """remove the prefix that Synapse queries add to field names"""
+    prefix, new_key = key.split('.', 1)
+    return new_key
 
 def clean_result(result):
     return {clean_key(key):unlist_singletons(value) for key,value in result.iteritems()}
 
-def clean_query_results(results):
-    return [clean_result(result) for result in results]
+def filter_keys(dictionary, keys_to_remove):
+    return {key:value for key,value in dictionary.iteritems() if key not in keys_to_remove}
 
-def remove_properties(result):
-    return {key:value for key,value in result.iteritems() if key not in property_keys}
+def clean_query_results(results, keys_to_remove=[]):
+    return [filter_keys(clean_result(result), keys_to_remove) for result in results]
 
+def query2df(queryContent, keepSynapseFields=set(['id', 'name'])):
+    """Converts the returned query object from Synapse into a Pandas DataFrame
+    
+    Arguments:
+    - `queryContent`: content returned from query or chunkedQuery
+    - `keepSynapseFields`: Synapse properties are removed from query results except those named here
+    """
+    return pd.DataFrame(clean_query_results(queryContent,
+        keys_to_remove=set(k for k in property_keys if k not in set(keepSynapseFields))))
 
 def extract_uuid(name):
     ## example: 0e90fb64-00b2-4b53-bbc7-df8182b84060
@@ -76,150 +62,163 @@ def extract_uuid(name):
         return None
 
 
-def get_annotations(folder_id, prefix=".somatic"):
-    synapse_ids   = []
-    sample_ids    = []
-    variant_types = []
-    results = syn.chunkedQuery('select * from entity where parentId=="%s" order by name' % folder_id)
-    for result in clean_query_results(results):
+DATA_SOURCE_PROJECT_ID = "syn2351328"
+PROJECT_SYNAPSE_ID     = "syn2875157"
+THIS_SCRIPT_SYNAPSE_ID = "syn3159349"
+BAR_CHART_SYNAPSE_ID   = "syn3158825"
+TABLE_SYNAPSE_ID       = "syn3159075"
 
-        ## assign variant type or give up
-        if result['name'].endswith(prefix+".indel.vcf.gz"):
-            variant_type = "indel"
-        elif result['name'].endswith(prefix+".snv_mnv.vcf.gz"):
-            variant_type = "snv"
-        elif result['name'].endswith(prefix+".sv.vcf.gz"):
-            variant_type = "sv"
-        else:
-            continue
 
-        ## assign sample id or give up
-        sample_id = extract_uuid(result['name'])
-        if 'sample_id' in result:
-            if sample_id and sample_id != result['sample_id']:
-                raise Exception("Sample ID {sample_id} doesn't match name {name}".format(**result))
-            if not sample_id:
-                sample_id = result['sample_id']
-        if not sample_id:
-            continue
-        synapse_id = result['id']
+class Source(object):
+    def __init__(self, name, folder_id):
+        self.name = name
+        self.folder_id = folder_id
+    def __repr__(self):
+        return "Source(name=\"{name}\", folder_id=\"{folder_id}\")".format(**self.__dict__)
 
-        if 'variant_type' in result and variant_type != result['variant_type'].lower():
-            raise Exception("Variant type {variant_type} doesn't match name {name}!".format(**result))
+sources = [
+#          Center        Folder
+    Source("Broad",      "syn3165121"),
+    Source("BSC",        "syn3165143"),
+    Source("DKFZ",       "syn3104289"),
+    Source("EMBL",       "syn3153529"),
+    Source("McGill",     "syn3165151"),
+    Source("MDA_HGSC",   "syn3167886"),
+    Source("MDA_KChen",  "syn3165149"),
+    Source("OICR",       "syn3167076"),
+#    Source("SANGER",     "syn3155834"), ## ???
+    Source("SFU",        "syn3165152"),
+    Source("UCSC",       "syn3107237"),
+    Source("WUSTL",      "syn3165146"),
+#    Source("Yale",       "syn3165120")  ## can't tell if these are germline or tumor
+    ]
 
-        synapse_ids.append(synapse_id)
-        sample_ids.append(sample_id)
-        variant_types.append(variant_type)
-        print synapse_id, sample_id, variant_type
 
-    return pd.DataFrame({
-            "synapse_id": synapse_ids,
-            "sample_id":  sample_ids,
-            "variant_type": variant_types})
-
+## Once the table is save in Synapse, summary stats can be computed with queries:
+## SELECT sample_id, variant_type, count(*) FROM syn3159075 GROUP BY sample_id, variant_type
+## SELECT source, workflow, variant_type, count(*) FROM syn3159075 GROUP BY source, workflow, variant_type
 
 def create_metadata_df(sources):
-
     dfs = []
     for source in sources:
-        print "\n\n", "-"*60, "\n", source.name
-        df = get_annotations(source.folder_id, source.prefix)
+        results = syn.chunkedQuery('select * from file where parentId=="%s"' % source.folder_id)
+        df = query2df(results)
+        print source.name, df.shape
         df['source'] = source.name
-        df['workflow'] = source.workflow
         dfs.append(df)
 
-    return pd.concat(dfs)
+    df_all = pd.concat(dfs, ignore_index=True)
+
+    ## find duplicates where an equivalent .vcf and .vcf.gz file both exist
+    def is_dup(name):
+        return name.endswith('.vcf') and (name+".gz" in df_all.name.values)
+    dups = df_all.name.apply(is_dup)
+
+    ## assume wustle VCFs are somatic variants only ??
+    df_all.call_type[ df_all.center=='wustl' ] = 'somatic'
+    df_all.workflow_name[ df_all.center=='wustl' ] = 'wustl'
+
+    ## fix DKFZ dataSubType
+    df_all.dataSubType[ df_all.center=='DKFZ' ] = df_all.dataType[ df_all.center=='DKFZ' ]
+
+    ## fix EMBL dataSubType and workflow
+    df_all.dataSubType[ df_all.center=='EMBL' ] = 'sv'
+    df_all.workflow_name[ df_all.center=='EMBL' ] = 'pcawg-delly'
+
+    df_all.dataSubType[ df_all.dataSubType=='indels' ] = 'indel'
+
+    df = df_all.ix[ (~df_all.center.isnull()) & (df_all.fileType=='vcf') & (df_all.call_type=='somatic') & (~dups), :]
+
+    df_progress = df.ix[:,['analysis_id_tumor', 'id', 'dataSubType', 'source', 'workflow_name']]
+    df_progress.columns = ['sample_id', 'synapse_id', 'variant_type', 'source', 'workflow']
+
+    return df_all, df, df_progress
+
+
+def add_new_rows_to_table(df, replace_table=False):
+    """Add rows for synapse IDs not already represented in the table or replace the whole table"""
+    schema = syn.get(TABLE_SYNAPSE_ID)
+    if replace_table:
+        ## delete previous entries in pilot-63-progress table
+        results = syn.tableQuery('select * from %s' % utils.id_of(schema), resultsAs='rowset')
+        syn.delete(results)
+    else:
+        results = syn.tableQuery('select synapse_id from %s' % utils.id_of(schema), includeRowIdAndRowVersion=False)
+        synapse_ids = [row[0] for row in results]
+        df = df[ [synapse_id not in synapse_ids for synapse_id in df['synapse_id']] ]
+
+    if df.shape[0] > 0:
+        return syn.store(Table(schema, df))
+    else:
+        return None
 
 
 def plot_progress(df, image_filename):
+    #plt.close()
+
     width = 0.8
-    variant_types = ['snv', 'sv', 'indel']
-    palette = {'snv':'#336699', 'sv':'#993333', 'indel':'#669933'}
+    padding = 0.2
+    variant_types = ['snv_mnv', 'sv', 'indel']
+    palette = {'snv_mnv':'#336699', 'sv':'#993333', 'indel':'#669933'}
 
     counts = []
     colors = []
     labels = []
     variant_type_indexes = {}
     for source in sources:
+        workflows = df_progress.workflow[df_progress.source==source.name].unique()
         for i, variant_type in enumerate(variant_types):
-            count = len(df[ (df['source']==source.name) &
-                            (df['workflow']==source.workflow) &
-                            (df['variant_type']==variant_type) ])
-            if count > 0:
-                variant_type_indexes[variant_type] = len(counts)
-                counts.append(count)
-                colors.append(palette[variant_type])
-                if source.workflow == source.name:
-                    labels.append(source.name)
-                else:
-                    labels.append(source.workflow + "\n" + source.name)
+            for workflow in workflows:
+                count = len(df[ (df['source']==source.name) &
+                                (df['workflow']==workflow) &
+                                (df['variant_type']==variant_type) ])
+                if count > 0:
+                    variant_type_indexes[variant_type] = len(counts)
+                    counts.append(count)
+                    colors.append(palette[variant_type])
+                    if workflow == source.name:
+                        labels.append(source.name)
+                    else:
+                        labels.append(source.name + " - " + workflow)
 
     ind = np.arange(len(counts))    # the x locations for the bars
-    bar = plt.bar(ind, counts, width=width, color=colors, alpha=0.6)
+    bar = plt.bar(ind+padding, counts, width=width, color=colors, alpha=0.6)
 
     plt.title('Count of VCF files by source and variant type')
-    plt.xticks(ind+width/2., labels)
+    #plt.xticks(ind+width/2., labels)
+    for i, label in enumerate(labels):
+        plt.text(i+width/2+padding, 3, str(label), rotation='vertical',
+            horizontalalignment='center',
+            verticalalignment='bottom')
 
     ## build a legend with a color patch for each variant type
-    plt.legend([bar[variant_type_indexes[v]] for v in variant_types], variant_types)
+    plt.legend([bar[variant_type_indexes[v]] for v in variant_types], variant_types,
+        loc='upper right', bbox_to_anchor=(1.05, 0.6))
 
     ## label each bar with its count
     for i,count in enumerate(counts):
-        plt.text(i+width/2, count+1,str(count), horizontalalignment="center")
+        plt.text(i+width/2+padding, count+1,str(count), horizontalalignment="center")
+
+    plt.xlim([0,23.2])
+    #plt.show()
 
     fig = plt.gcf()
     fig.set_size_inches(14,9)
     fig.savefig(image_filename, bbox_inches='tight')
 
 
-def create_table_schema(project, activity):
-    cols = [
-        Column(name='sample_id',  columnType='STRING', maximumSize=36),
-        Column(name='synapse_id', columnType='STRING', maximumSize=13),
-        Column(name='variant_type', columnType='STRING', maximumSize=10),
-        Column(name='source', columnType='STRING', maximumSize=30),
-        Column(name='workflow', columnType='STRING', maximumSize=100)
-    ]
 
-    schema = syn.store(
-        Schema(name='pilot-63-progress', 
-               columns=cols,
-               parent=project,),
-        activity=activity)
-    print "Schema created:", schema.id
+def update_figure_and_table(sources, script_commit_url=None, replace_table=False):
+    df_all, df, df_progress = create_metadata_df(sources)
+    print df_progress.groupby(["source", "variant_type"])["synapse_id"].count()
 
-    return schema, activity
-
-
-def add_new_rows_to_table(schema, df):
-    results = syn.tableQuery('select synapse_id from %s' % utils.id_of(schema), includeRowIdAndRowVersion=False)
-    synapse_ids = [row[0] for row in results]
-    df_new_rows = df[ [synapse_id not in synapse_ids for synapse_id in df['synapse_id']] ]
-    if df_new_rows.shape[0] > 0:
-        return syn.store(Table(schema, df_new_rows))
-    else:
-        return None
-
-def store_this_script():
-    return syn.store(
-        File("https://github.com/cbare/pcawg-pilot-63-validation/",
-             name="pilot-63-progress.py",
-             parentId="syn2875157",
-             synapseStore=False),
-        used="https://github.com/cbare/pcawg-pilot-63-validation/")
-
-def update_figure_and_table(sources, script_commit_url=None):
-    df = create_metadata_df(sources)
-    print df.groupby(["source", "variant_type"])["synapse_id"].count()
-
-    schema = syn.get(TABLE_SYNAPSE_ID)
-    table = add_new_rows_to_table(schema, df)
+    table = add_new_rows_to_table(df_progress, replace_table)
 
     if table:
         script_entity = syn.get(THIS_SCRIPT_SYNAPSE_ID, downloadFile=False)
         if script_commit_url:
             script_entity.externalURL = script_commit_url
-            script_entity = syn.store(script_entity)
+            #script_entity = syn.store(script_entity)
 
         activity = Activity(
             name='Pilot-63-progress',
@@ -227,7 +226,7 @@ def update_figure_and_table(sources, script_commit_url=None):
             used=list(set(source.folder_id for source in sources)),
             executed=[script_entity])
 
-        activity = syn.setProvenance(schema, activity)
+        activity = syn.setProvenance(table.schema, activity)
 
         image_filename="pilot-63-progress.png"
         plot_progress(df, image_filename)
@@ -235,9 +234,8 @@ def update_figure_and_table(sources, script_commit_url=None):
         bar_chart = syn.get(BAR_CHART_SYNAPSE_ID, downloadFile=False)
         bar_chart.path = "pilot-63-progress.png"
         bar_chart.synapseStore=True
-        bar_chart = syn.store(bar_chart, activity=activity)
+        #bar_chart = syn.store(bar_chart, activity=activity)
 
-    return bar_chart
 
 
 def main():
@@ -248,6 +246,7 @@ def main():
     parser.add_argument("-u", "--user", help="UserName", default=None)
     parser.add_argument("-p", "--password", help="Password", default=None)
     parser.add_argument("--debug", help="Show verbose error output from Synapse API calls", action="store_true", default=False)
+    parser.add_argument("--replace-table", help="Replace whole pilot-63-progress table", action="store_true", default=False)    
     parser.add_argument("script_commit_url", metavar="SCRIPT_COMMIT_URL", help="Github URL of this script")
 
     args = parser.parse_args()
@@ -259,7 +258,7 @@ def main():
         args.password = os.environ.get('SYNAPSE_PASSWORD', None)
     syn.login(email=args.user, password=args.password)
 
-    update_figure_and_table(sources, args.script_commit_url)
+    update_figure_and_table(sources, args.script_commit_url, args.replace_table)
 
 
 
